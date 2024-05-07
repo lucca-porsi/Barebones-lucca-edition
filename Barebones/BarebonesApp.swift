42e5b44570a37486a8c081e6e96a265db75ce3f7
lucca-porsi <l.porsi-carey-23@student.lboro.ac.uk>
2024-05-07 16:58:47 +0100

    Initial Commit

---
+++ Barebones/BarebonesApp.swift
@@ -0,0 +1,18 @@
+//
+//  BarebonesApp.swift
+//  Barebones
+//
+//  Created by (s) Lucca Porsi Porsi-Carey on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarebonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
